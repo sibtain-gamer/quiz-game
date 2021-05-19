9cd984626698d8a5c308fb5f542c0875546aff32
sibtain-gamer <72454273+sibtain-gamer@users.noreply.github.com>
2021-05-19 09:04:15 +0530

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,28 @@
+
+
+
+print('Do you want to play this game? Made By Sibtain.exe or Gamer Sibtain ')
+Answer = input()
+if Answer == 'yes':
+    print('welcome to the game')
+else:
+    print("haha you still have to play!!!")
+
+ans = input('1. who is the owner and founder of Tesla Motors? ')
+if ans == 'elon musk':
+    print('welldone')
+else:
+    print('I think you live on mars')
+
+ans = input('2. who is the biggest individual youtuber? ')
+if ans == 'pewdiepie' or  ans == 'pewds':
+    print('noice, bro fist to you')
+else:
+    print('you suck!')
+
+ans = input('3. Give this quiz a rating between 1 and 5 ')
+if ans == '5' or  ans == 'five' or ans == '4' or ans == 'four':
+    print('I dont think this code desreves that rating')
+else:
+    print('You muffin head, how dare you give me a bad rating')
+
